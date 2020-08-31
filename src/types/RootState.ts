c627f53490740824e24805363bff1634aa8bb316
Leonardo <leodaiub@gmail.com>
2020-08-31 10:45:10 -0300

    Initialize project using Create React App

---
+++ src/types/RootState.ts
@@ -0,0 +1,13 @@
+import { AuthState } from 'app/containers/Auth/types';
+import { DesignsState } from 'app/containers/Designs/types';
+// [IMPORT NEW CONTAINERSTATE ABOVE] < Needed for generating containers seamlessly
+
+/* 
+  Because the redux-injectors injects your reducers asynchronously somewhere in your code
+  You have to declare them here manually
+*/
+export interface RootState {
+  auth?: AuthState;
+  designs?: DesignsState;
+  // [INSERT NEW REDUCER KEY ABOVE] < Needed for generating containers seamlessly
+}
